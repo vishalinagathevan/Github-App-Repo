a08e110dc4b9fb10d0dbee32004cd169d29251d9
vishalinagathevan <126974938+vishalinagathevan@users.noreply.github.com>
2023-12-13 15:47:33 +0530

    commited

---
+++ for_loop.py
@@ -0,0 +1,26 @@
+# # num = [12345,6789]
+# # for x in num:
+#     # print(num[0])
+
+# for x in range(6):
+#     print(x)    
+
+# # print(num[0])
+    
+    
+    
+# if the given number is 10
+given = 3
+ 
+# set up a variable to store the sum
+# with initial value of 0
+sum = 0
+ 
+# since we want to include the number 10 in the sum
+# increment given number by 1 in the for loop
+for i in range(1,given_number+1):
+        sum+=i
+ 
+# print the total sum at the end
+print(sum)    
+    
